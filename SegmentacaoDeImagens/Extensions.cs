254ae5f386162d0508b72e072d50a62cf60077af
Yan Paulo <yanpaulo@hotmail.com>
2018-08-04 10:05:02 -0300

    Crescimento de Regiões

---
+++ SegmentacaoDeImagens/Extensions.cs
@@ -0,0 +1,34 @@
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SegmentacaoDeImagens
+{
+    public static class Extensions
+    {
+        public static IEnumerable<Point> Vizinhos<T>(this Image<T, byte> img, Point point) where T : struct, IColor
+        {
+            if (point.X > 0)
+            {
+                yield return new Point(point.X - 1, point.Y);
+            }
+            if (point.X < img.Size.Width - 1)
+            {
+                yield return new Point(point.X + 1, point.Y);
+            }
+            if (point.Y > 0)
+            {
+                yield return new Point(point.X, point.Y - 1);
+            }
+            if (point.Y < img.Size.Height - 1)
+            {
+                yield return new Point(point.X, point.Y + 1);
+            }
+        }
+    }
+}
--- SegmentacaoDeImagens/Program.cs
+++ SegmentacaoDeImagens/Program.cs
@@ -10,6 +10,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using static Emgu.CV.CvInvoke;
+using static System.Math;
 
 namespace SegmentacaoDeImagens
 {
@@ -17,9 +18,64 @@ namespace SegmentacaoDeImagens
     {
         static void Main(string[] args)
         {
-            HoughCirclesDetect();
         }
 
+        private static void Crescimento(string filename = @"C:\Users\yanpa\source\repos\SegmentacaoDeImagens\SegmentacaoDeImagens\bin\Debug\images\3026_lg.jpg")
+        {
+            const int Limiar = 15;
+            const int Canal = 2;
+            var img = new Image<Bgr, byte>(filename);
+
+            var percent = 10;
+            var size = img.Size;
+            var centro = new Point(size.Width / 2, size.Height / 2);
+
+            var seed = new Rectangle(
+                    new Point(centro.X - size.Width * percent / 2 / 100, centro.Y - size.Height * percent / 2 / 100),
+                    new Size(size.Width * percent / 100, size.Height * percent / 100));
+
+            var gray = img.Convert<Gray, byte>();
+            var map = new byte[size.Width, size.Height, 3];
+            var queue = new Queue<Point>();
+
+            for (int i = seed.Left; i <= seed.Right; i++)
+            {
+                for (int j = seed.Top; j <= seed.Bottom; j++)
+                {
+                    map[i, j, Canal] = 255;
+                    queue.Enqueue(new Point(i, j));
+                }
+            }
+
+            //Instrução fora do while pois gray.Data tem complexidade significativa - O(1).
+            var data = gray.Data;
+
+            while (queue.Any())
+            {
+                var proximo = queue.Dequeue();
+                var vizinhos = img.Vizinhos(proximo);
+
+                var pixel = data[proximo.X, proximo.Y, 0];
+
+                foreach (var v in vizinhos)
+                {
+                    if (map[v.X, v.Y, Canal] != 255)
+                    {
+                        var diff = Math.Abs(pixel - data[v.X, v.Y, 0]);
+                        if (diff <= Limiar)
+                        {
+                            queue.Enqueue(v);
+                            map[v.X, v.Y, Canal] = 255;
+                        }
+                    }
+                }
+            }
+
+            var dest = new Image<Bgr, byte>(map);
+            dest.Save("dest.jpg");
+        }
+
+        #region Hough
         private static void HoughCirclesDetect()
         {
             const string path = "images";
@@ -73,5 +129,6 @@ namespace SegmentacaoDeImagens
             circle.Center.X - circle.Radius < 0 ||
             circle.Center.Y + circle.Radius > image.Height ||
             circle.Center.Y - circle.Radius < 0;
+        #endregion
     }
 }
